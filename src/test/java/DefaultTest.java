df441b4a5246d7b8f5fd565eaf08f84a8e6e0dcd
Ancozockt Enviroment <support@ancozockt.de>
2021-12-05 12:51:10 +0100

    Start TestSuite

---
+++ src/test/java/DefaultTest.java
@@ -0,0 +1,11 @@
+import de.ancozockt.advent.days.Day1;
+import org.junit.jupiter.api.Test;
+
+public class DefaultTest {
+
+    @Test
+    public void defaultTest() {
+        assert true;
+    }
+
+}
