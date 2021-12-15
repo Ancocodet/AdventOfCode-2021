f6b933ccbb7e5f4ba5d0fcb3992c4d08aa20a683
dseydel <dseydel@united-internet.de>
2021-12-15 11:14:06 +0100

    Solve Day 15

--- src/main/java/de/ancozockt/advent/days/Day15.java
+++ src/main/java/de/ancozockt/advent/days/Day15.java
@@ -4,13 +4,11 @@ import de.ancozockt.advent.interfaces.AdventDay;
 import de.ancozockt.advent.utilities.ADay;
 
 import java.io.BufferedReader;
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @ADay(day = "day15")
 public class Day15 implements AdventDay {
@@ -23,33 +21,46 @@ public class Day15 implements AdventDay {
     @Override
     public String part1(BufferedReader reader) {
         long[][] grid = formatInput(reader);
-        return String.valueOf(findPath(grid, grid.length - 1,grid[0].length - 1 , 1));
+        return String.valueOf(findPath(grid));
     }
 
     @Override
     public String part2(BufferedReader reader) {
         long[][] grid = formatInput(reader);
-        return String.valueOf(findPath(grid, (grid.length * 5) - 1,(grid[0].length * 5) - 1 , 5) - 1);
+        grid = transformGrid(grid, 5);
+        return String.valueOf(findPath(grid));
     }
 
-    private long findPath(long[][] grid, int col, int row, int multiplier){
-        long[][] dist = new long[grid.length * multiplier][grid[0].length * multiplier];
+    private long[][] transformGrid(long[][] currentGrid, int multiplier){
+        long[][] newGrid = new long[(currentGrid.length * multiplier)][(currentGrid[0].length * multiplier)];
 
-        int[] dx = { 1, 0 };
-        int[] dy = { 0, 1 };
+        for(int x = 0; x < newGrid.length; x++){
+            for(int y = 0; y < newGrid[0].length; y++){
+                long gridNum = currentGrid[x % currentGrid.length][y % currentGrid[0].length] + (x / currentGrid.length) + (y / currentGrid[0].length);
+                if (gridNum > 9) {
+                    gridNum -= 9;
+                }
+                newGrid[x][y] = gridNum;
+            }
+        }
+
+        return newGrid;
+    }
+
+    private long findPath(long[][] grid){
+        long[][] dist = new long[grid.length][grid[0].length];
+
+        int[] dx = { 1, 0, -1, 0};
+        int[] dy = { 0, 1, 0, -1};
 
         for(int x = 0; x < dist.length; x++){
             for(int y = 0; y < dist[0].length; y++){
-                dist[x][y] = Integer.MAX_VALUE;
+                dist[x][y] = Long.MAX_VALUE;
             }
         }
         dist[0][0] = 0;
 
-        PriorityQueue<Chiton> queue = new PriorityQueue<>(row * col, (o1, o2) -> {
-            if(o1.distance < o2.distance) return -1;
-            if(o1.distance > o2.distance) return 1;
-            return 0;
-        });
+        PriorityQueue<Chiton> queue = new PriorityQueue<>((grid.length-1) * (grid[0].length - 1), Comparator.comparingLong(o -> o.distance));
 
         queue.add(new Chiton(0, 0, 0));
         while (!queue.isEmpty()){
@@ -58,21 +69,15 @@ public class Day15 implements AdventDay {
                 int rows = current.x + dx[i];
                 int columns = current.y + dy[i];
                 if(rows >= 0 && rows < dist.length && columns >= 0 && columns < dist[0].length){
-                    long gridNum = (grid[rows % grid.length][columns % grid.length] + (rows/dist.length)) % 10;
-                    if (dist[rows][columns] > dist[current.x][current.y] + gridNum)
-                    {
-                        if (dist[rows][columns] != Integer.MAX_VALUE)
-                        {
-                            Chiton adj = new Chiton(rows, columns,
-                                    dist[rows][columns]);
+                    long gridNum = grid[rows % grid.length][columns % grid.length] + (rows/grid.length) + (columns/grid[0].length);
+
+                    if (dist[rows][columns] > dist[current.x][current.y] + gridNum) {
+                        if (dist[rows][columns] != Long.MAX_VALUE) {
+                            Chiton adj = new Chiton(rows, columns, dist[rows][columns]);
                             queue.remove(adj);
                         }
-
-                        dist[rows][columns] = dist[current.x][current.y] +
-                                gridNum;
-
-                        queue.add(new Chiton(rows, columns,
-                                dist[rows][columns]));
+                        dist[rows][columns] = dist[current.x][current.y] + gridNum;
+                        queue.add(new Chiton(rows, columns, dist[rows][columns]));
                     }
                 }
             }
